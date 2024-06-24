82e73ec1324eb6ac534bad1e7b4bf591c98101a4
ShamsheerKhan <shamsheerkhan055@gmail.com>
2024-06-24 16:18:06 +0530

    x-index: valve changed in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
 import GetTheApp from './GetTheApp';
-import newTlogo from '../assets/newTlogo.png';
+import newlogo from '../assets/newlogo.png';
 
 function Header() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            <img src={newTlogo} alt="aayani" className="logo-img" />
+            <img src={newlogo} alt="newlogo" className="logo-img" />
           </a>
+          <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
